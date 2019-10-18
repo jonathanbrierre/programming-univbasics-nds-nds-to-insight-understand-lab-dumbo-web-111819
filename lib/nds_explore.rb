9d80a28517a81cd860b8bea70c9a671a83cda38e
jonathan brierre <jonathanbrierre@yahoo.com>
2019-10-18 02:31:37 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -11,7 +11,14 @@ def pretty_print_nds(nds)
 end
 
 def print_first_directors_movie_titles
- 
+ director_index = 0 
+   while director_index < nds.length do 
+     this_movie = 0 
+     while this_movie <nds[director_index][:movies].length do
+       puts nds[director_index][:movies][this_movie][:title]
+       this_movie += 1 
+     end
+   end
 
   
 end
